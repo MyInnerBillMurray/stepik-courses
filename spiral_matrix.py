a3376b6c719b91779d6bd84e8d43c2aac26a0e8b
MyInnerBillMurray <alexleftysbass@gmail.com>
2021-02-22 12:15:16 +0300

    67 course tasks, spiral matrix

--- spiral_matrix.py
+++ spiral_matrix.py
@@ -7,29 +7,27 @@ for j in range(n):
     flag += 1
     matrix[0][j] += flag
 
-s1 = 0
-s3 = 0
+step = 0
 
 for numbers in range(n**2 + 1):
     n -= 1
-    for i in range(s1, n):
+    for i in range(step, n):
         flag += 1
         matrix[i + 1][j] += flag
 
-    for j in range(n, s3, -1):
+    for j in range(n, step, -1):
         flag += 1
         matrix[i + 1][j - 1] += flag
 
-    for i in range(n, s3 + 1, -1):
+    for i in range(n, step + 1, -1):
         flag += 1
         matrix[i - 1][j - 1] += flag
 
-    for j in range(s1 + 1, n):
+    for j in range(step + 1, n):
         flag += 1
         matrix[i - 1][j] += flag
 
-    s1 += 1
-    s3 += 1
+    step += 1
 
 for elements in matrix:
     print(*elements)
